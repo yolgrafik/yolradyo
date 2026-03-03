9c36ff8bd9f99066f7a3e0dd62d37426bd1eae8f
Ali Celik <alicelikx02@gmail.com>
2026-03-03 21:01:01 +0100

    Refactor web routes to utilize FrontendController for improved organization and maintainability. Added routes for various frontend pages including programlar, haberler, videolar, galeri, and more.

---
+++ app/Http/Controllers/FrontendController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class FrontendController extends Controller
+{
+    public function home()
+    {
+        return view('frontend.home');
+    }
+
+    public function programlar()
+    {
+        return view('frontend.page', ['pageTitle' => 'Programlar']);
+    }
+
+    public function haberler()
+    {
+        return view('frontend.page', ['pageTitle' => 'Haberler']);
+    }
+
+    public function videolar()
+    {
+        return view('frontend.page', ['pageTitle' => 'Video Galeri']);
+    }
+
+    public function galeri()
+    {
+        return view('frontend.page', ['pageTitle' => 'Foto Galeri']);
+    }
+
+    public function reklam()
+    {
+        return view('frontend.page', ['pageTitle' => 'Reklam & Isbirligi']);
+    }
+
+    public function hakkimizda(string $slug)
+    {
+        $titles = [
+            'biz-kimiz' => 'Biz Kimiz',
+            'misyon' => 'Misyon & Vizyon',
+            'politika' => 'Yayin Politikamiz',
+        ];
+        $pageTitle = $titles[$slug] ?? 'Hakkimizda';
+        return view('frontend.page', ['pageTitle' => $pageTitle]);
+    }
+
+    public function iletisim()
+    {
+        return view('frontend.page', ['pageTitle' => 'Iletisim']);
+    }
+
+    public function gizlilik()
+    {
+        return view('frontend.page', ['pageTitle' => 'Gizlilik Politikasi']);
+    }
+
+    public function cerez()
+    {
+        return view('frontend.page', ['pageTitle' => 'Cerez Politikasi']);
+    }
+
+    public function kullanim()
+    {
+        return view('frontend.page', ['pageTitle' => 'Kullanim Sartlari']);
+    }
+
+    public function kvkk()
+    {
+        return view('frontend.page', ['pageTitle' => 'KVKK Aydinlatma Metni']);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FrontendController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [FrontendController::class, 'home']);
+Route::get('/programlar', [FrontendController::class, 'programlar']);
+Route::get('/haberler', [FrontendController::class, 'haberler']);
+Route::get('/videolar', [FrontendController::class, 'videolar']);
+Route::get('/galeri', [FrontendController::class, 'galeri']);
+Route::get('/reklam', [FrontendController::class, 'reklam']);
+Route::get('/hakkimizda', fn () => redirect('/hakkimizda/biz-kimiz'));
+Route::get('/hakkimizda/{slug}', [FrontendController::class, 'hakkimizda'])->where('slug', 'biz-kimiz|misyon|politika');
+Route::get('/iletisim', [FrontendController::class, 'iletisim']);
+Route::get('/gizlilik', [FrontendController::class, 'gizlilik']);
+Route::get('/cerez', [FrontendController::class, 'cerez']);
+Route::get('/kullanim', [FrontendController::class, 'kullanim']);
+Route::get('/kvkk', [FrontendController::class, 'kvkk']);
 
 Route::prefix('admin')->group(function () {
     Route::get('', function () {
