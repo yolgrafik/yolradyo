23e78075ecd2455a3343d7db0de601bf8c250187
Ali Celik <alicelikx02@gmail.com>
2026-03-04 20:09:28 +0100

    Add menu management routes and update navigation in admin layout

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -901,6 +901,7 @@
                             <li><a href="{{ route('admin.settings.seo') }}" class="nav-item {{ request()->routeIs('admin.settings.seo') ? 'is-active' : '' }}">SEO Ayarlari</a></li>
                             <li><a href="{{ route('admin.settings.social') }}" class="nav-item {{ request()->routeIs('admin.settings.social') ? 'is-active' : '' }}">Sosyal Medya Linkleri</a></li>
                             <li><a href="{{ route('admin.settings.footer') }}" class="nav-item {{ request()->routeIs('admin.settings.footer') ? 'is-active' : '' }}">Footer Yonetimi</a></li>
+                            <li><a href="{{ route('admin.menu.index') }}" class="nav-item {{ request()->routeIs('admin.menu.*') ? 'is-active' : '' }}">Menu Yonetimi</a></li>
                             <li><a href="{{ route('admin.settings.theme') }}" class="nav-item {{ request()->routeIs('admin.settings.theme') ? 'is-active' : '' }}">Tema & Renk Ayarlari</a></li>
                         </ul>
                     </div>
--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,16 @@ Route::prefix('admin')->group(function () {
     Route::get('settings/theme', [App\Http\Controllers\Admin\SettingsController::class, 'themeForm'])->name('admin.settings.theme');
     Route::post('settings/theme', [App\Http\Controllers\Admin\SettingsController::class, 'saveTheme']);
 
+        Route::prefix('menu')->name('admin.menu.')->group(function () {
+            Route::get('/', [App\Http\Controllers\Admin\MenuController::class, 'index'])->name('index');
+            Route::get('create', [App\Http\Controllers\Admin\MenuController::class, 'create'])->name('create');
+            Route::post('/', [App\Http\Controllers\Admin\MenuController::class, 'store'])->name('store');
+            Route::post('reorder', [App\Http\Controllers\Admin\MenuController::class, 'reorder'])->name('reorder');
+            Route::get('{menu}/edit', [App\Http\Controllers\Admin\MenuController::class, 'edit'])->name('edit');
+            Route::put('{menu}', [App\Http\Controllers\Admin\MenuController::class, 'update'])->name('update');
+            Route::delete('{menu}', [App\Http\Controllers\Admin\MenuController::class, 'destroy'])->name('destroy');
+        });
+
         Route::middleware('admin.permission:users.manage')->prefix('users')->name('admin.users.')->group(function () {
             Route::get('/', [App\Http\Controllers\Admin\AdminUserController::class, 'index'])->name('index');
             Route::get('create', [App\Http\Controllers\Admin\AdminUserController::class, 'create'])->name('create');
