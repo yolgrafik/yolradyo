ad7c13654c02936dac540f75c1744407266e61fc
Ali Celik <alicelikx02@gmail.com>
2026-03-05 21:40:58 +0100

    Refactor ticker margin-top for enhanced layout consistency

--- resources/views/partials/programcilar-section-only.blade.php
+++
@@ -1,8 +0,0 @@
-@php
-    $programcilar = $programcilar ?? collect();
-@endphp
-@if($programcilar->isNotEmpty())
-<div class="container-fluid px-3 px-lg-4 mt-4">
-    @include('partials.programcilar-cards')
-</div>
-@endif
