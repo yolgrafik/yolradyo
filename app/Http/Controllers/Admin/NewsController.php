9eab73b2bc7f9346ecb5dcd45bfe7ec42eb2e8f8
yolgrafik <yolgrafik46@gmail.com>
2026-03-07 22:04:31 +0100

    Remove unused NewsMedia model import from NewsController to streamline code.

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\News;
-use App\Models\NewsMedia;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Str;
